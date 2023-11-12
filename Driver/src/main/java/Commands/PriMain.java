4557ef6aa32e55fd882a7cc5a15d172f47a5885c
ARGamer36 <ajrodz2004@gmail.com>
2023-11-12 00:16:53 -0500

    Initial Commit

---
+++ Driver/src/main/java/Commands/PriMain.java
@@ -0,0 +1,11 @@
+package Commands;
+
+import Main.MainCommands;
+
+public class PriMain extends MainCommands {
+    public PriMain(String version) {
+        super(version);
+        //prefixCommands.add();
+        //slashCommands.add();
+    }
+}
---
+++ Driver/src/main/java/PriBot.java
@@ -0,0 +1,27 @@
+import Commands.PriMain;
+import Main.BotDriver;
+import net.dv8tion.jda.api.entities.Activity;
+
+import javax.security.auth.login.LoginException;
+
+public class PriBot extends BotDriver {
+    @Override
+    protected void setup() {
+        VERSION = "1.0";
+        activity = Activity.watching("Bots Being Developed");
+        TOKEN = "PRI_BOT";
+    }
+
+    public PriBot() throws LoginException {
+        super();
+        shardManager.addEventListener(new PriMain(VERSION));
+    }
+
+    public static void main(String[] args) {
+        try {
+            PriBot bot = new PriBot();
+        } catch (LoginException e) {
+            System.out.println("ERROR: provided bot token invalid");
+        }
+    }
+}
